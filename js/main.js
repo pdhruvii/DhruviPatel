abd087b519846d1dc57308a553e2e80974fe4995
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2023-11-13 02:11:08 -0500

    Modified Home Page

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*===========Sections Active Link===========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+
+    });
+}
+
+/*===========Typed.js Automated Text===========*/
+document.addEventListener('DOMContentLoaded', function () {
+    const typed = new Typed('.automated-text', {
+        strings: [
+            '<span style="color: #c3073f;">ideas into reality.</span>',
+        ],
+        typeSpeed: 75,
+        backSpeed: 75,
+        backDelay: 500,
+        loop: true,
+        smartBackspace: true,
+        cursorChar: '<span style="color: #c3073f;">|</span>',
+    });
+});
+
+  
