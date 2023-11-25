0670cdf2e5ca36d50a1596341e19120289737ece
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2023-11-25 15:28:01 -0500

    Modified About Me Page

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ window.onscroll = () => {
     });
 }
 
-/*===========Typed.js Automated Text===========*/
+/*===========Typed.js Automated Text Home Catch Phrase===========*/
 document.addEventListener('DOMContentLoaded', function () {
     const typed = new Typed('.automated-text', {
         strings: [
@@ -33,6 +33,22 @@ document.addEventListener('DOMContentLoaded', function () {
         cursorChar: '<span style="color: #c3073f;">|</span>',
     });
 });
+
+/*===========Typed.js Automated Text for About Me Tag===========*/
+document.addEventListener('DOMContentLoaded', function () {
+    const typed = new Typed('.about-automated-text', {
+        strings: [
+            '<span style="color: #c3073f;">New Grad Roles for 2024!</span>',
+        ],
+        typeSpeed: 75,
+        backSpeed: 75,
+        backDelay: 500,
+        loop: true,
+        smartBackspace: true,
+        cursorChar: '<span style="color: #c3073f;">|</span>',
+    });
+});
+
 
 // Get the current window width
 var windowWidth = window.innerWidth;
