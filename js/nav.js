b5f2c18a49bf6c285f064789f46ce4820260f4bb
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2023-12-29 21:33:32 -0500

    Mod indiv files

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.hamburger');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    console.log(visibility);
+    if (visibility === "false"){
+        primaryNav.setAttribute('data-visible', true);
+    }
+    else{
+        primaryNav.setAttribute('data-visible', false);
+    }
+})
