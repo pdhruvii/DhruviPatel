dc6b75bdbb8eb92199f28d5ea450a19ceb1be2af
pdhruvii <92284156+pdhruvii@users.noreply.github.com>
2024-07-07 19:53:18 -0400

    About me w/o responsiveness

--- newmain.js
+++ newmain.js
@@ -12,3 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
         cursorChar: '<span style="color: #c3073f;">|</span>',
     });
 });
+
+/*===========Typed.js Automated Text for About Me Tag===========*/
+document.addEventListener('DOMContentLoaded', function () {
+    const typed = new Typed('.about-automated-text', {
+        strings: [
+            '<span style="color: #c3073f;">New Grad Roles for 2024!</span>',
+        ],
+        typeSpeed: 75,
+        backSpeed: 75,
+        backDelay: 500,
+        loop: true,
+        smartBackspace: true,
+        cursorChar: '<span style="color: #c3073f;">|</span>',
+    });
+});
